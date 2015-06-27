2b16a498528d6b872e1dd09c51d7f5d8b8e4d2a2
kauedb <kauedb@gmail.com>
2015-06-27 19:28:31 -0300

    salesman-0 comment removed

--- src/main/java/br/com/kproj/salesman/infrastructure/configuration/SpringConfig.java
+++ src/main/java/br/com/kproj/salesman/infrastructure/configuration/SpringConfig.java
@@ -2,12 +2,9 @@ package br.com.kproj.salesman.infrastructure.configuration;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 @Configuration
-//@EnableLoadTimeWeaving
 @EnableSpringConfigured
 public class SpringConfig {
 }
