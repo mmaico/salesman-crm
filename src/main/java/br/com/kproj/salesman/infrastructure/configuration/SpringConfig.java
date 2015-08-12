dc4c28dd503183196e5fc00e7f8ae35bd5d91dbb
mmaco <mmaico@gmail.com>
2015-08-12 10:05:45 -0300

    Alteracao de configuracao para o resource location

--- src/main/java/br/com/kproj/salesman/infrastructure/configuration/SpringConfig.java
+++ src/main/java/br/com/kproj/salesman/infrastructure/configuration/SpringConfig.java
@@ -16,10 +16,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 @ComponentScan("br.com.kproj")
 public class SpringConfig extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter {
 
+    /**
+     * TODO: colocar resource location do properties.
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
         registry.addResourceHandler("/view-resource/**")
-                   .addResourceLocations("file:/Users/marcelomaicodejesus/workspace_salesman/facility/");
+                   .addResourceLocations("file:/opt/templates/");
     }
 }
