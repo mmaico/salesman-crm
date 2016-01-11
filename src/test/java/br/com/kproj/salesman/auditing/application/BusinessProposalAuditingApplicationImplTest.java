f01b44336512d7292bb55d1276ca915f1af63c3b
mmaico <mmaico@gmail.com>
2016-01-11 12:50:18 -0200

    alteracao da versao do powermock.

---
+++ src/test/java/br/com/kproj/salesman/auditing/application/BusinessProposalAuditingApplicationImplTest.java
@@ -0,0 +1,10 @@
+package br.com.kproj.salesman.auditing.application;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by mmaico on 07/01/16.
+ */
+public class BusinessProposalAuditingApplicationImplTest {
+
+}
