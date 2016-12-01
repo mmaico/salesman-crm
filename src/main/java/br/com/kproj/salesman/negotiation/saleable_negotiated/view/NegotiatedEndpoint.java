eb008006a0b790fe2e4900e03b9904219cf5ad24
Marcelo Maico <mmaico@gmail.com>
2016-12-01 01:06:13 -0200

    Criacao de endpoint para listagem de items negociados.

---
+++ src/main/java/br/com/kproj/salesman/negotiation/saleable_negotiated/view/NegotiatedEndpoint.java
@@ -0,0 +1,95 @@
+package br.com.kproj.salesman.negotiation.saleable_negotiated.view;
+
+import br.com.kproj.salesman.infrastructure.exceptions.NotFoundException;
+import br.com.kproj.salesman.infrastructure.http.response.handler.resources.ResourceItem;
+import br.com.kproj.salesman.infrastructure.http.response.handler.resources.ResourceItems;
+import br.com.kproj.salesman.negotiation.saleable_negotiated.application.SaleableNegotiatedFacade;
+import br.com.kproj.salesman.negotiation.saleable_negotiated.domain.model.negotiated.Negotiated;
+import br.com.kproj.salesman.negotiation.saleable_negotiated.domain.model.negotiation.Negotiation;
+import br.com.kproj.salesman.negotiation.saleable_negotiated.view.support.builders.NegotiatedResourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class NegotiatedEndpoint {
+
+
+    private SaleableNegotiatedFacade service;
+
+    private NegotiatedResourceBuilder builder;
+
+
+    @Autowired
+    public NegotiatedEndpoint(SaleableNegotiatedFacade service, NegotiatedResourceBuilder builder) {
+        this.service = service;
+        this.builder = builder;
+    }
+
+
+    @RequestMapping(value = "/rs/customers/negotiations/{negotiationId}/negotiated-items", method = RequestMethod.GET)
+    public @ResponseBody
+    ResourceItems list(@PathVariable Long negotiationId) {
+
+       Negotiation negotiation = new Negotiation(negotiationId);
+       Iterable<Negotiated> negotiateds = service.findAll(negotiation);
+
+       return builder.build(negotiateds);
+    }
+
+    @RequestMapping(value = "/rs/customers/negotiations/negotiated-items/{negotiatedId}", method = RequestMethod.GET)
+    public @ResponseBody
+    ResourceItem getOne(@PathVariable Long negotiatedId) {
+
+        Optional<Negotiated> negotiatedOptional = service.getOne(negotiatedId);
+        Negotiated negotiated = negotiatedOptional.orElseThrow(NotFoundException::new);
+
+        return builder.build(negotiated);
+    }
+//
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @RequestMapping(value = "/rs/customers/{customerId}/negotiations", method = RequestMethod.POST)
+//    public @ResponseBody
+//    ResourceItem create(@PathVariable Long customerId, @RequestBody NegotiationResource resource) {
+//
+//        Negotiation negotiation = NegotiationBuilder.createNegotiation()
+//                .withCustomer(customerId)
+//                .withRegion(resource.getRegionId())
+//                .withCareOf(resource.getCareOf())
+//                .withDeliveryForeCast(resource.getDeliveryForeCast())
+//                .withIntroduction(resource.getIntroduction()).build();
+//
+//        Seller seller = createSeller(resource.getSellerId()).build();
+//
+//        Optional<Negotiation> negotiationSaved = service.register(createNegotiation(seller, negotiation));
+//
+//        return builder.build(negotiationSaved.get());
+//    }
+//
+//    @ResponseStatus(HttpStatus.OK)
+//    @RequestMapping(value = "/rs/customers/negotiations/{negotiationId}", method = RequestMethod.PUT)
+//    public @ResponseBody
+//    ResourceItem update(@PathVariable Long negotiationId, @RequestBody NegotiationResource resource) {
+//
+//        Negotiation negotiation = NegotiationBuilder.createNegotiation(negotiationId)
+//                    .withRegion(resource.getRegionId())
+//                    .withCareOf(resource.getCareOf())
+//                    .withDeliveryForeCast(resource.getDeliveryForeCast())
+//                    .withIntroduction(resource.getIntroduction())
+//                    .withAmmountPayable(resource.getAmmountPayable())
+//                    .withDiscount(resource.getDiscount())
+//                    .withTemperature(resource.getTemperature())
+//                    .withCustomer(resource.getCustomerId())
+//                    .withSeller(resource.getSellerId())
+//                .build();
+//
+//        updateFields.addFieldsToUpdate(negotiation);
+//
+//        Negotiation negotiationSaved = service.update(negotiation);
+//
+//        return builder.build(negotiationSaved);
+//    }
+
+    
+}
---
+++ src/main/java/br/com/kproj/salesman/negotiation/saleable_negotiated/view/support/builders/NegotiatedResourceBuilder.java
@@ -0,0 +1,56 @@
+package br.com.kproj.salesman.negotiation.saleable_negotiated.view.support.builders;
+
+
+import br.com.kproj.salesman.infrastructure.http.response.handler.resources.ResourceHolder;
+import br.com.kproj.salesman.infrastructure.http.response.handler.resources.ResourceItem;
+import br.com.kproj.salesman.infrastructure.http.response.handler.resources.ResourceItems;
+import br.com.kproj.salesman.negotiation.saleable_negotiated.domain.model.negotiated.Negotiated;
+import br.com.kproj.salesman.negotiation.saleable_negotiated.view.support.resources.NegotiatedResource;
+import br.com.uol.rest.apiconverter.ConverterToResource;
+import br.com.uol.rest.infrastructure.libraries.ContextArguments;
+import com.google.common.collect.Lists;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static br.com.kproj.salesman.infrastructure.http.response.handler.resources.ResourceHolder.getUri;
+import static br.com.uol.rest.infrastructure.libraries.SelectableArguments.createEmpty;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+
+@Component("negotiatedResourceBuilderNegotiatedModule")
+public class NegotiatedResourceBuilder {
+
+    @Autowired
+    private HttpServletRequest request;
+
+
+    public ResourceItem build(Negotiated negotiated) {
+        NegotiatedResource resource = buildItem(negotiated);
+
+        return new ResourceItem(resource, getUri(request));
+    }
+
+    public ResourceItems build(Iterable<Negotiated> negotiateds) {
+
+        List<NegotiatedResource> resources = Lists.newArrayList(negotiateds).stream()
+                .map(item -> buildItem(item)).collect(Collectors.toList());
+
+        ResourceItems resourceItems = new ResourceItems(resources, getUri(request));
+        ResourceHolder.setInfoPageable(negotiateds, resourceItems);
+
+        return resourceItems;
+    }
+
+    public NegotiatedResource buildItem(Negotiated negotiated) {
+        ContextArguments context = ContextArguments.create(createEmpty(), EMPTY);
+
+        NegotiatedResource resource = new NegotiatedResource();
+
+        ConverterToResource.convert(negotiated, resource, context);
+        return resource;
+    }
+
+}
